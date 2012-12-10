3c47321a647f47af353e83d06824b7c17e40944b
RoyCurtis <r.curtis@metaverse.com>
2012-12-10 01:08:25 +0000

    initial commit, VPServices 0.4:
    - singleton-centric design (subject to change)
    - jump module
    - telegram module
    - usermanager that tracks teleport history
    - user monitor to track user entries/exits for statistics
    - build monitor to track object creates/modifies for stats

---
+++ Services/Jumps.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using VpNet.Core;
+using VpNet.Core.EventData;
+using VpNet.Core.Structs;
+
+namespace VPServices.Services
+{
+    struct Jump
+    {
+        public string Name;
+        public float X;
+        public float Y;
+        public float Z;
+        public float Yaw;
+        public static Jump Empty = new Jump { Name = "" };
+
+        public static Jump FromString(string dat)
+        {
+            if (dat == "" || !dat.Contains(","))
+                throw new ArgumentNullException();
+
+            var parts = dat.Split(new[] { "," }, StringSplitOptions.None);
+            return new Jump
+            {
+                Name = parts[0],
+                X = float.Parse(parts[1]),
+                Y = float.Parse(parts[2]),
+                Z = float.Parse(parts[3]),
+                Yaw = float.Parse(parts[4]),
+            };
+        }
+
+        public string Save()
+        {
+            return string.Join(",",
+                Name,
+                X, Y, Z, Yaw);
+        }
+    }
+
+    struct SpawnedJump
+    {
+        public int Id;
+        public DateTime When;
+    }
+
+    class Jumps
+    {
+        public const string JUMPS = "Jumps.dat";
+        public const string JUMP_ID = "!!!svcjump";
+        public const string JUMP_DESC = "Click to teleport to {0}! (disappears after 30 seconds, say !back for a jump back)";
+        public const string JUMPBACK_DESC = "Click to teleport back! (disappears after 30 seconds)";
+        public const string JUMP_ACTION = "create color b, scale 2; activate teleportxyz {0} {1} {2} {3}; " + JUMP_ID;
+
+        public const string JUMPLIST_DESC = "This list will disappear after 30 seconds:\n{0}";
+        public const string JUMPLIST_ACTION = "create sign bcolor=b color=orange, scale 2;" + JUMP_ID;
+
+        List<Jump> storedJumps = new List<Jump>();
+        List<SpawnedJump> spawnedJumps = new List<SpawnedJump>();
+
+        public Jumps()
+        {
+            // Load all saved telegrams
+            if (File.Exists(JUMPS))
+                foreach (var jump in File.ReadAllLines(JUMPS))
+                    storedJumps.Add(Jump.FromString(jump));
+        }
+
+        public void Update()
+        {
+            try
+            {
+                foreach (var spawned in spawnedJumps)
+                    if (DateTime.Now.Subtract(spawned.When).TotalSeconds > 30)
+                    {
+                        Console.WriteLine("Jumps: Deleting spawned jump, {0}", spawned.Id);
+                        VPServices.Bot.DeleteObject(new VpObject { Id = spawned.Id });
+                        spawnedJumps.Remove(spawned);
+                    }
+            }
+            catch { }
+        }
+
+        public void SaveJumps()
+        {
+            File.WriteAllLines(JUMPS,
+                from t in storedJumps
+                select t.Save()
+                , Encoding.UTF8);
+        }
+
+        public void CmdAddJump(Instance bot, Avatar who, string data)
+        {
+            var name = data.Trim().ToLower();
+            if (name == "" || name == "random") return;
+
+            if (getJump(name).Name != "")
+            {
+                bot.Say(string.Format("{0}: Jump already exists", who.Name));
+                return;
+            }
+
+            var x = (float)Math.Round(who.X, 2);
+            var y = (float)Math.Round(who.Y, 2);
+            var z = (float)Math.Round(who.Z, 2);
+            storedJumps.Add(new Jump { Name = name, X = x, Y = y, Z = z, Yaw = who.Yaw });
+            bot.Say(string.Format("{0}: Saved a jump at {1}, {2}, {3} for {4}",
+                who.Name,
+                who.X, who.Y, who.Z,
+                name));
+
+            Console.WriteLine("Saved a jump for {0} at {1}, {2}, {3} for {4}",
+                who.Name,
+                who.X, who.Y, who.Z,
+                name);
+            SaveJumps();
+        }
+
+        public void CmdDelJump(Instance bot, Avatar who, string data)
+        {
+            var name = data.Trim().ToLower();
+            if (name == "" || name == "random") return;
+
+            var jump = getJump(name);
+            if (jump.Name == "")
+            {
+                bot.Say(string.Format("{0}: Jump does not exist", who.Name));
+                return;
+            }
+            else storedJumps.Remove(jump);
+            bot.Say(string.Format("{0}: Jump deleted", who.Name));
+            Console.WriteLine("Deleted {0} jump for {1}", name, who.Name);
+            SaveJumps();
+        }
+
+        public void CmdSpawnJump(Instance bot, Avatar who, string data)
+        {
+            var name = data.Trim().ToLower();
+            if (name == "") return;
+
+            var jump = (name == "random")
+                //? storedJumps[new Random().Next(0, storedJumps.Count)]
+                ? getJump(name)
+                : getJump(name);
+            if (jump.Name == name)
+            {
+                bot.CallbackObjectAdd += OnJumpAddCallback;
+                bot.AddObject(new VpObject
+                {
+                    Model = "zcomp1.rwx",
+                    Description = string.Format(JUMP_DESC, jump.Name),
+                    Action = string.Format(JUMP_ACTION, jump.X, jump.Y, jump.Z, jump.Yaw),
+                    Position = new Vector3(who.X, who.Y + 0.025f, who.Z)
+                });
+            }
+            else
+                bot.Say(string.Format("{0}: No such jump", who.Name));
+
+            return;
+        }
+
+        public void CmdListJump(Instance bot, Avatar who, string data)
+        {
+            var list = string.Join(",\n",
+                from j in storedJumps
+                select j.Name);
+
+            bot.CallbackObjectAdd += OnJumpAddCallback;
+            bot.AddObject(new VpObject
+            {
+                Model = "button_4.rwx",
+                Description = string.Format(JUMPLIST_DESC, list),
+                Action = string.Format(JUMPLIST_ACTION),
+                Position = new Vector3(who.X, who.Y + 0.025f, who.Z)
+            });
+
+            return;
+        }
+
+        public void OnJumpAddCallback(Instance bot, int id)
+        {
+            Console.WriteLine("Jumps: Spawned jump monitored, {0}", id);
+            spawnedJumps.Add(new SpawnedJump { Id = id, When = DateTime.Now });
+            bot.CallbackObjectAdd -= OnJumpAddCallback;
+        }
+
+        Jump getJump(string name)
+        {
+            foreach (var jump in storedJumps)
+                if (jump.Name == name) return jump;
+
+            return Jump.Empty;
+        }
+    }
+}
---
+++ Services/Telegrams.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using VpNet.Core;
+using VpNet.Core.EventData;
+using VpNet.Core.Structs;
+
+namespace VPServices.Services
+{
+    class Telegram
+    {
+        public string To;
+        public string From;
+        public string Message;
+        public bool Sent;
+
+        public static Telegram FromString(string dat)
+        {
+            if (dat == "" || !dat.Contains(","))
+                return null;
+
+            var parts = dat.Split(new[] { "," }, StringSplitOptions.None);
+            return new Telegram
+            {
+                To = parts[0],
+                From = parts[1],
+                Message = parts[2].Replace("%COMMA", ","),
+                Sent = bool.Parse(parts[3]),
+            };
+        }
+
+        public string Save()
+        {
+            return string.Join(",",
+                To,
+                From,
+                Message.Replace(",", "%COMMA"),
+                Sent);
+        }
+    }
+
+    class Telegrams
+    {
+        const string TGRAMS = "Telegrams.dat";
+        const string TBLOCKED = "DoNotTelegram.dat";
+
+        List<Telegram> StoredGrams = new List<Telegram>();
+        List<string> DoNotTelegram = new List<string>();
+
+        public Telegrams()
+        {
+            // Load all saved telegrams
+            if (File.Exists(TGRAMS))
+                foreach (var tgram in File.ReadAllLines(TGRAMS))
+                    StoredGrams.Add(Telegram.FromString(tgram));
+
+            // Load block list
+            if (File.Exists(TBLOCKED))
+                DoNotTelegram = new List<string>(File.ReadAllLines(TBLOCKED));
+
+            VPServices.Bot.EventChat += checkTelegrams;
+        }
+
+        public void SaveTelegrams()
+        {
+            File.WriteAllLines(TGRAMS,
+                from t in StoredGrams
+                select t.Save()
+                , Encoding.UTF8);
+        }
+
+        public void OnCommand(Instance bot, Chat chat, string data)
+        {
+            var matches = Regex.Match(data, "^(.+?): (.+?)$", RegexOptions.IgnoreCase);
+            if (!matches.Success) return;
+            
+            var target = matches.Groups[1].Value.ToLower().Trim();
+            var msg = matches.Groups[2].Value.Trim();
+
+            if (DoNotTelegram.Contains(target))
+            {
+                bot.Say(string.Format("{0}: {1} is not accepting telegrams.", chat.Username, target));
+                return;
+            }
+
+            StoredGrams.Add(new Telegram { From = chat.Username, To = target, Message = msg });
+            bot.Say(string.Format("{0}: Saved for {1}", chat.Username, target));
+            Console.WriteLine("Telegram by {0} recorded for {1}.", chat.Username, target);
+            SaveTelegrams();
+        }
+
+        public void Block(Instance bot, string name)
+        {
+            if (DoNotTelegram.Contains(name))
+            {
+                bot.Say(string.Format("{0}: You are now accepting telegrams.", name));
+                DoNotTelegram.Remove(name);
+            }
+            else
+            {
+                bot.Say(string.Format("{0}: You are now blocking telegrams.", name));
+                DoNotTelegram.Add(name);
+            }
+
+            File.WriteAllLines(TBLOCKED, DoNotTelegram, Encoding.UTF8);
+        }
+
+        void checkTelegrams(Instance bot, Chat chat)
+        {
+            if (DoNotTelegram.Contains(chat.Username.ToLower())) return;
+
+            bool sentOne = false;
+            foreach (var tg in StoredGrams)
+                if (!tg.Sent && chat.Username.ToLower() == tg.To)
+                {
+                    bot.Say(string.Format("{0}: You have a telegram from {1}: {2}", chat.Username, tg.From, tg.Message));
+                    tg.Sent = true;
+                    sentOne = true;
+                    SaveTelegrams();
+                }
+
+            if (sentOne) bot.Say("(Say '!telegram <recepient>: <message>' to respond or !blocktelegrams to prevent further)");
+        }
+    }
+}
